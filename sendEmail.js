721fdb3a25af66f0336973899cc93b9c88bcc9d9
MarketSpace11 <134568340+MarketSpace11@users.noreply.github.com>
2024-11-05 08:56:35 -0800

    sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,17 +1,19 @@
-// Inicializa EmailJS con tu User ID
-emailjs.init("xPw6PF0ef-Di5oFKO");  // Reemplaza con tu propio User ID
+$(document).ready(function() {
+    // Inicializa EmailJS con tu User ID
+    emailjs.init("xPw6PF0ef-Di5oFKO");
 
-// Agrega un evento 'submit' al formulario
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Previene la recarga de la página
+    // Agrega un evento 'submit' al formulario
+    $('#contactForm').on('submit', function(event) {
+        event.preventDefault(); // Previene la recarga de la página
 
-    // Envía el formulario usando EmailJS
-    emailjs.sendForm('service_xbrsts6', 'template_eht2dzq', this)  // Reemplaza con tus IDs de servicio y plantilla
-        .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-            alert('Message sent successfully');
-        }, function(error) {
-            console.log('FAILED...', error);
-            alert('Error sending message');
-        });
+        // Envía el formulario usando EmailJS
+        emailjs.sendForm('service_xbrsts6', 'template_eht2dzq', this)
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Message sent successfully');
+            }, function(error) {
+                console.log('FAILED...', error);
+                alert('Error sending message');
+            });
+    });
 });
